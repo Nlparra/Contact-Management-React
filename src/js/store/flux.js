c0295ec474e4f61ec24af004f989c0ec2d1c2bc5
Nelson <nlparra.dev@gmail.com>
2020-04-10 01:05:10 +0000

    work on post fetch - need to link to component

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,3 +1,5 @@
+import { AddContact } from "../views/AddContact";
+
 const url = "https://assets.breatheco.de/apis/fake/contact/";
 
 const getState = ({ getStore, setStore }) => {
@@ -12,6 +14,30 @@ const getState = ({ getStore, setStore }) => {
 					.then(data => {
 						console.log("Get Contact", data), setStore({ Contact: data });
 					});
+			},
+
+			addContact(name, phone, email, address) {
+				fetch(url, {
+					method: "Post",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						email: email,
+						address: address,
+						agenda_slug: "NelsonContact"
+					})
+				}).then(() => {
+					fetch(url + "/agenda/NelsonContact")
+						.then(res => res.json())
+						.then(data => {
+							console.log("Get Contact", data),
+								setStore({
+									Contact: data
+								});
+						})
+						.catch(e => console.error(e));
+				});
 			}
 		}
 	};
